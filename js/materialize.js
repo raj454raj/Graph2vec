a546ba53bd13dfe003000245d46fd021c35533ba
raj454raj <raj454raj@gmail.com>
2016-04-14 14:16:57 +0530

    Webpage almost done. Results left

--- js/materialize.js
+++ js/materialize.js
@@ -700,7 +700,7 @@ if ($) {
       $body.width(oldWidth);
 
       var defaults = {
-        opacity: 0.5,
+        opacity: 0.8,
         in_duration: 350,
         out_duration: 250,
         ready: undefined,
@@ -1960,7 +1960,7 @@ $(document).ready(function(){
         }
         else {
           // Insert as text;
-          toast.innerHTML = html; 
+          toast.innerHTML = html;
         }
         // Bind hammer
         var hammerHandler = new Hammer(toast, {prevent_default: false});
@@ -2539,9 +2539,9 @@ $(document).ready(function(){
 		    var offset = $(this.hash).offset().top + 1;
 
 //          offset - 200 allows elements near bottom of page to scroll
-			
+
 	    	$('html, body').animate({ scrollTop: offset - 200 }, {duration: 400, queue: false, easing: 'easeOutCubic'});
-			
+
 		  });
 		});
 		options = options || {
@@ -2742,7 +2742,6 @@ $(document).ready(function(){
         $(this).addClass('tabbed');
         var $this = $(this);
         $this.one('blur', function(e) {
-          
           $(this).removeClass('tabbed');
         });
         return;
